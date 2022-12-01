1082cad84e1969ca00ef8c679e7679bbd0851656
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-12-01 20:43:26 +0100

    solved day 1

---
+++ cmake/day_1.cmake
@@ -0,0 +1,2 @@
+add_executable(day_1 src/day_1.cpp)
+add_test(NAME day_1 COMMAND day_1 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/input)
---
+++ src/day_1.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ifstream in("day_1");
+
+  std::string      line;
+  int              sum = 0;
+  std::vector<int> sums;
+  while (std::getline(in, line)) {
+    if (line.empty()) {
+      sums.push_back(sum);
+      sum = 0;
+    } else {
+      sum += std::stoi(line);
+    }
+  }
+
+  std::sort(sums.begin(), sums.end());
+
+  int tot = sums[sums.size() - 1] + sums[sums.size() - 2] + sums[sums.size() - 3];
+
+  cout << tot << endl;
+
+  return 1;
+}
