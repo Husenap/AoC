4f54d103539724509cfab5b4aef7b67f61c5107b
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-12-09 21:32:10 +0100

    solved day 8

---
+++ src/day_8.cpp
@@ -0,0 +1,74 @@
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ifstream in("day_8");
+
+  std::vector<std::string> lines;
+  std::string              line;
+  while (std::getline(in, line)) {
+    lines.push_back(line);
+  }
+
+  const int rows = lines.size();
+  const int cols = lines[0].size();
+
+  int tot             = rows * cols;
+  int bestScenicScore = 0;
+
+  for (int y = 0; y < rows; ++y) {
+    for (int x = 0; x < cols; ++x) {
+      int covered = 0;
+      int score   = 1;
+      int steps   = 0;
+      for (int ny = y - 1; ny >= 0; --ny) {
+        ++steps;
+        if (lines[ny][x] >= lines[y][x]) {
+          ++covered;
+          break;
+        }
+      }
+      score *= steps;
+      steps = 0;
+      for (int ny = y + 1; ny < rows; ++ny) {
+        ++steps;
+        if (lines[ny][x] >= lines[y][x]) {
+          ++covered;
+          break;
+        }
+      }
+      score *= steps;
+      steps = 0;
+      for (int nx = x - 1; nx >= 0; --nx) {
+        ++steps;
+        if (lines[y][nx] >= lines[y][x]) {
+          ++covered;
+          break;
+        }
+      }
+      score *= steps;
+      steps = 0;
+      for (int nx = x + 1; nx < cols; ++nx) {
+        ++steps;
+        if (lines[y][nx] >= lines[y][x]) {
+          ++covered;
+          break;
+        }
+      }
+      score *= steps;
+      bestScenicScore = std::max(bestScenicScore, score);
+      if (covered == 4) --tot;
+    }
+  }
+  cerr << "visible trees: " << tot << endl;
+  cerr << "best scenic score: " << bestScenicScore << endl;
+
+  return 1;
+}
