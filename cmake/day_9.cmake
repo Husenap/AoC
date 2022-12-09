37aa465d1b604c0c58a7b93ebcea140db72c2d19
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-12-09 21:55:07 +0100

    solved day 9

---
+++ cmake/day_9.cmake
@@ -0,0 +1,2 @@
+add_executable(day_9 src/day_9.cpp)
+add_test(NAME day_9 COMMAND day_9 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/input)
---
+++ src/day_9.cpp
@@ -0,0 +1,62 @@
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+using Pos = std::pair<int, int>;
+
+static const std::map<char, Pos> Delta = {{
+    {'L', {-1, 0}},
+    {'D', {0, -1}},
+    {'U', {0, 1}},
+    {'R', {1, 0}},
+}};
+
+int main() {
+  ifstream in("day_9");
+
+  {
+    std::array<Pos, 10> Ps;
+    std::set<Pos>       visited;
+    visited.insert(Ps.back());
+
+    char dir;
+    int  steps;
+    while (in >> dir >> steps) {
+      const auto delta = Delta.at(dir);
+      while (steps--) {
+        Ps[0].first += delta.first;
+        Ps[0].second += delta.second;
+        for (int i = 1; i < Ps.size(); ++i) {
+          auto& previous = Ps[i - 1];
+          auto& current  = Ps[i];
+
+          if (std::abs(current.first - previous.first) > 1 ||
+              std::abs(current.second - previous.second) > 1) {
+            if (current.first < previous.first) {
+              ++current.first;
+            }
+            if (current.second < previous.second) {
+              ++current.second;
+            }
+            if (current.first > previous.first) {
+              --current.first;
+            }
+            if (current.second > previous.second) {
+              --current.second;
+            }
+          }
+        }
+        visited.insert(Ps.back());
+      }
+    }
+    cerr << "tail visited: " << visited.size() << endl;
+  }
+
+  return 1;
+}
